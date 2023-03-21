7ef75bc479fad063641ca9bdb4c774c80998c26b
Breno <brenobcsilva@gmail.com>
2023-03-21 14:07:56 -0300

    first commit

--- src/main/java/com/example/alunos/controller/AlunoController.java
+++ src/main/java/com/example/alunos/controller/AlunoController.java
@@ -1,4 +1,17 @@
 package com.example.alunos.controller;
 
+import com.example.alunos.persistence.model.Aluno;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AlunoController {
+
+    @GetMapping("/mostrarAluno")
+    private Aluno aluno(){
+        return new Aluno(1L,"Breno","Silva");
+    }
+
+
+
 }
---
+++ src/main/java/com/example/alunos/persistence/model/Aluno.java
@@ -0,0 +1,46 @@
+package com.example.alunos.persistence.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String sobrenome;
+
+    public Aluno(Long id, String nome, String sobrenome) {
+        this.id = id;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+}
